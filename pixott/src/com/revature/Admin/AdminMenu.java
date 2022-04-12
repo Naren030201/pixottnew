cc4a51fd879827aae5b367ecc83b21b31739d790
Naren030201 <92349708+Naren030201@users.noreply.github.com>
2022-04-12 23:45:34 +0530

    Add files via upload

---
+++ pixott/src/com/revature/Admin/AdminMenu.java
@@ -0,0 +1,18 @@
+package com.revature.Admin;
+
+import com.revature.app.App;
+
+public class AdminMenu {
+public static void display() {
+	System.out.println("Menu");
+	System.out.println("===");
+	System.out.println("1) Change Top 5 Movies");
+	System.out.println("2) Mark movie unavailable");
+	System.out.println("3) Modify movie details");
+	System.out.println("Select an option:");
+	int option = App.scanner.nextInt();
+	if(option==1) {
+		
+	}
+}
+}
